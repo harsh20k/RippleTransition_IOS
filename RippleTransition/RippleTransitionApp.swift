5e3b2daa7fe6c94b11465367b204cb313e458ff4
harsh <harsh.pandey77@gmail.com>
2025-02-27 14:00:51 +0530

    Initial Commit

---
+++ RippleTransition/RippleTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleTransitionApp.swift
+//  RippleTransition
+//
+//  Created by harsh  on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RippleTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

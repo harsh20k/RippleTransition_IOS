619e8e9d43bd4ed127b1245137151308db63306a
harsh <harsh.pandey77@gmail.com>
2025-02-27 18:19:58 +0530

    Reverse ripple effect added

--- RippleTransition/ContentView.swift
+++ RippleTransition/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
 	@State private var count: Int = 0
 	@State private var rippleLocation : CGPoint = .zero
 	
+	//Reverse ripple properties
+	@State private var showOverlayView: Bool = false
+	@State private var overlayRippleLocation: CGPoint = .zero
+	
     var body: some View {
 		NavigationStack {
 			VStack {
@@ -33,7 +37,47 @@ struct ContentView: View {
 					}
 				}
 			}
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.overlay(alignment: .bottomTrailing) {
+				GeometryReader {
+					let frame = $0.frame(in: .global)
+					
+					Button {
+						overlayRippleLocation = .init(x: frame.midX, y: frame.midY)
+						
+						withAnimation(.linear(duration: 1)) {
+							showOverlayView = true
+						}
+					} label: {
+						Image(systemName: "square.and.arrow.up.fill")
+							.font(.title2)
+							.foregroundStyle(.white)
+							.frame(width: 50, height: 50)
+							.background(.indigo.gradient, in: .circle)
+							.contentShape(.rect)
+					}
+				}
+				.frame(width: 50, height: 50)
+				.padding(15)
+			}
 			.navigationTitle("Ripple Transition")
+		}
+		.overlay {
+			if showOverlayView {
+				ZStack {
+					Rectangle()
+						.fill(.indigo.gradient)
+						.ignoresSafeArea()
+						.onTapGesture {
+							withAnimation(.easeInOut(duration: 0.55)) {
+								showOverlayView = false
+							}
+						}
+					
+					Text("Tap Anywhere to dismiss")
+				}
+				.transition(.reverseRipple(location: overlayRippleLocation))
+			}
 		}
     }
 	

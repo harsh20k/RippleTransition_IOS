9b1758e79b4d2e024427703462b1bfd055b08f12
harsh <harsh.pandey77@gmail.com>
2025-02-27 15:29:09 +0530

    initial commit

---
+++ RippleTransition/Helpers/Ripple.swift
@@ -0,0 +1,18 @@
+//
+//  Ripple.swift
+//  RippleTransition
+//
+//  Created by harsh  on 27/02/25.
+//
+
+import SwiftUI
+
+struct Ripple: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ripple()
+}
